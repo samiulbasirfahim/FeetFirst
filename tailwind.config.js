619010d0ed1fb11808b33c32784d303ae2b3cf94
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2025-08-23 23:12:15 +0600

    barebone setup done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            colors: {
+                background: "#1A1C1B",
+                "muted-background": "#303231",
+                primary: "#62A07B",
+                accent: "#404241",
+                foreground: "#BAC4C6",
+                "muted-foreground": "#585C5B",
+            },
+        },
+    },
+    plugins: [],
+};
