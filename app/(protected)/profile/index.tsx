408d45cc6d76064ddf8105be2b98ed62c75ef22f
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2025-08-29 07:01:28 +0600

    fix: items center in profile page

--- app/(protected)/profile/index.tsx
+++ app/(protected)/profile/index.tsx
@@ -105,19 +105,19 @@ export default function Screen() {
                 <View className="flex-row items-center gap-2">
                     <Button
                         variant="outline"
-                        className="rounded-full bg-muted-background/50 border-2 border-muted-background h-10 w-10 p-0 justify-center"
+                        className="rounded-full bg-muted-background/50 border-2 border-muted-background h-10 w-10 p-0 justify-center items-center"
                     >
                         <FB />
                     </Button>
                     <Button
                         variant="outline"
-                        className="rounded-full bg-muted-background/50 border-2 border-muted-background h-10 w-10 p-0 justify-center"
+                        className="rounded-full bg-muted-background/50 border-2 border-muted-background h-10 w-10 p-0 justify-center items-center"
                     >
                         <INSTA />
                     </Button>
                     <Button
                         variant="outline"
-                        className="rounded-full bg-muted-background/50 border-2 border-muted-background h-10 w-10 p-0 justify-center"
+                        className="rounded-full bg-muted-background/50 border-2 border-muted-background h-10 w-10 p-0 justify-center items-center"
                     >
                         <WEB />
                     </Button>
