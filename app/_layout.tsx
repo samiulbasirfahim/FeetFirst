5f3c832402cc2300005d06463808c799ce9eae2e
Swadhin <swadhin.cse5.bu@gmail.com>
2025-09-03 06:35:24 +0600

    updating home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,57 +1,56 @@
-import { useAuthStore } from "@/store/auth";
-import { Stack, useRouter, useSegments } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Host } from "react-native-portalize";
-import "./global.css";
+import { useAuthStore } from '@/store/auth';
+import { Stack, useRouter, useSegments } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Host } from 'react-native-portalize';
+import './global.css';
 
 export default function RootLayout() {
-    const { user, onboarding_complete, setUser } = useAuthStore();
-    const router = useRouter();
-    const segments = useSegments();
+  const { user, onboarding_complete, setUser } = useAuthStore();
+  const router = useRouter();
+  const segments = useSegments();
 
-    const [isReady, setIsReady] = useState<boolean>(false);
-  
-    useEffect(() => {
-        if (isReady) {
-            router.replace("/(protected)/home/foot-exercise");
-            // const inProtectedRoute = segments[0] === "(public)";
-            // if (!user) {
-            //     router.replace("/(public)");
-            // } else if (!user.verified) {
-            //     router.replace("/(public)/register/otp-authenticattion");
-            // } else if (!onboarding_complete) {
-            //     router.replace("/on-boarding");
-            // } else if (!inProtectedRoute) {
-            //     router.replace("/(protected)");
-            // }
-        }
-    }, [isReady]);
+  const [isReady, setIsReady] = useState<boolean>(false);
 
+  useEffect(() => {
+    if (isReady) {
+      router.replace('/(protected)/home');
+      // const inProtectedRoute = segments[0] === "(public)";
+      // if (!user) {
+      //     router.replace("/(public)");
+      // } else if (!user.verified) {
+      //     router.replace("/(public)/register/otp-authenticattion");
+      // } else if (!onboarding_complete) {
+      //     router.replace("/on-boarding");
+      // } else if (!inProtectedRoute) {
+      //     router.replace("/(protected)");
+      // }
+    }
+  }, [isReady]);
 
-    useEffect(() => {
-        (async () => {
-            setUser({
-                email: "",
-                full_name: "",
-                verified: true,
-            });
+  useEffect(() => {
+    (async () => {
+      setUser({
+        email: '',
+        full_name: '',
+        verified: true,
+      });
 
-            setIsReady(true);
-        })();
-    }, []);
+      setIsReady(true);
+    })();
+  }, []);
 
-    return (
-        <GestureHandlerRootView
-            style={{
-                flex: 1,
-            }}
-        >
-            <Host>
-                <StatusBar style="light" />
-                <Stack screenOptions={{ headerShown: false }} />
-            </Host>
-        </GestureHandlerRootView>
-    );
+  return (
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+      }}
+    >
+      <Host>
+        <StatusBar style="light" />
+        <Stack screenOptions={{ headerShown: false }} />
+      </Host>
+    </GestureHandlerRootView>
+  );
 }
