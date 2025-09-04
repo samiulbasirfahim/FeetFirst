21fae662de21a7e8d734dcec3fa4ca59949b8b10
saruf <saruf700@gmail.com>
2025-09-05 00:34:29 +0600

    feat: skifiner @issues: card carousel, map

--- components/ui/carousel-home.tsx
+++ components/ui/carousel-home.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import { Image, View, Dimensions } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
-import { Typography } from './typography';
-import { useSharedValue } from 'react-native-reanimated';
-import Arrow from '@/assets/svgs/arrow-exercise.svg';
+import * as React from "react";
+import { Image, View, Dimensions } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+import { Typography } from "./typography";
+import { useSharedValue } from "react-native-reanimated";
+import Arrow from "@/assets/svgs/arrow-exercise.svg";
 
-import { SvgProps } from 'react-native-svg';
+import { SvgProps } from "react-native-svg";
 
 export type ShoeItem = {
   itemName: string;
   brandLogo: React.FC<SvgProps>;
   price: string;
-  image: string;
+  image: string | number;
   brandName: string;
 };
 
 function HomeCarausel({ shoes }) {
-  const { width } = Dimensions.get('window');
+  const { width } = Dimensions.get("window");
   const progress = useSharedValue<number>(0);
 
   const renderItem = ({ item }: { item: ShoeItem }) => (
@@ -29,7 +29,12 @@ function HomeCarausel({ shoes }) {
       }}
     >
       <View className="absolute z-50">
-        <Image source={item.image} style={{ width: 240, height: 240 }} />
+        <Image
+          source={
+            typeof item.image === "string" ? { uri: item.image } : item.image
+          }
+          style={{ width: 240, height: 240 }}
+        />
       </View>
       <View className="absolute right-5 top-2">
         <Typography className="font-medium text-foreground text-2xl leading-[24px]">
@@ -47,18 +52,14 @@ function HomeCarausel({ shoes }) {
           <item.brandLogo height={50} width={100} className="" />
         </View>
       </View>
-      <View className="absolute bottom-0 right-0 p-3  border border-primary rounded-2xl">
+      <View className="absolute bottom-0 right-0 p-3 border border-primary rounded-2xl">
         <Arrow />
       </View>
     </View>
   );
 
   return (
-    <View
-      id="carousel-component"
-      dataSet={{ kind: 'basic-layouts', name: 'parallax' }}
-      className="pl-3"
-    >
+    <View className="pl-3">
       <Carousel
         autoPlayInterval={2000}
         data={shoes}
@@ -69,7 +70,7 @@ function HomeCarausel({ shoes }) {
         height={230}
         style={{
           width: width,
-          borderColor: '#ffffff',
+          borderColor: "#ffffff",
         }}
         mode="parallax"
         modeConfig={{
